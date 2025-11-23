0c6b9aeaec769306912b573e06603d5f8711c735
kuzin <kuzin@procpb.ru>
2025-11-23 11:33:51 +0300

    Fix preamble detection error in Vite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
+        @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     </head>
 
